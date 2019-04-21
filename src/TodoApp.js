311030bbafdded914db311dddbcb8229d28c43e7
smw mc <smeredithwilliams@gmail.com>
2019-04-20 21:15:32 -0700

    added background img

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react'
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, ImageBackground } from "react-native"
 
 import AddTodo from './containers/AddTodo';
 import VisibleTodos from './containers/VisibleTodos';
+import BackGround from '../assets/rainbackground.jpg';
 
 class TodoApp extends Component {
 
     render() {
         return (
             <View style={styles.container}>
+            <ImageBackground source={BackGround} style={{width: '100%', height: '100%'}}>
                 <AddTodo />
 
                 <View>
                     <VisibleTodos />
                 </View>
+            </ImageBackground>
             </View>
         );
     }
